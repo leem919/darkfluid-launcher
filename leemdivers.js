1d3da8529a427f7e2ac789ab36f18f0d30be289d
leem919 <leem919@gmail.com>
2025-05-29 23:45:01 -0700

    Change to fixed static api

--- leemdivers.js
+++ leemdivers.js
@@ -1,60 +1,31 @@
-// Define hosts
+// Define addresses
 const originalHost = "https://api.live.prod.thehelldiversgame.com/";
-const replacementHost = "https://leemdivers-api-bxra.onrender.com/";
+const replacementHost = "https://dancing-starlight-d55eb8.netlify.app/";
+const loginEndpoint = "https://api.live.prod.thehelldiversgame.com/api/Account/Login";
+const loginRedirect = "https://dummyjson.com/c/3029-d29f-4014-9fb4";
 
-const deniedEndpoints = [
-    "https://api.live.prod.thehelldiversgame.com/api/FriendsV2/Block",
-    "https://api.live.prod.thehelldiversgame.com/api/Progression/Items/Customize",
-    "https://api.live.prod.thehelldiversgame.com/api/Monetization/Steam/RedeemStoreContent",
-    "https://api.live.prod.thehelldiversgame.com/api/Stats/profile/summary",
-    "https://api.live.prod.thehelldiversgame.com/api/Stats/profile/801/summary",
-    "https://api.live.prod.thehelldiversgame.com/api/Stats/war/801/summary",
-    "https://api.live.prod.thehelldiversgame.com/api/LeaderBoard/MiniGame/4171714171",
-    "https://api.live.prod.thehelldiversgame.com/api/Mail/inbox",
-    "https://api.live.prod.thehelldiversgame.com/api/v2/Assignment/Player",
-    "https://api.live.prod.thehelldiversgame.com/api/Operation/Create",
-    "https://api.live.prod.thehelldiversgame.com/api/Account/ReportPosition",
-    "https://api.live.prod.thehelldiversgame.com/api/Operation/Mission/Start",
-    "https://api.live.prod.thehelldiversgame.com/api/Operation/Mission/SetMaxRewards",
-    "https://api.live.prod.thehelldiversgame.com/api/Progression/inventory/consume",
-    "https://api.live.prod.thehelldiversgame.com/api/Progression/Achievements"
-];
-
-function isTargetUrl(url) {
-    return url.indexOf(originalHost) === 0;
+function getRedirectUrl(url) {
+    if (url === loginEndpoint) {
+        return loginRedirect;
+    } else if (url.indexOf(originalHost) === 0) {
+        return url.replace(originalHost, replacementHost);
+    }
+    return null;
 }
 
-function replaceUrl(url) {
-    return url.replace(originalHost, replacementHost);
-}
-
-function isDeniedEndpoint(url) {
-    return deniedEndpoints.some(endpoint => url.startsWith(endpoint));
-}
-
-// Find the curl_easy_setopt function
+// Hook into curl_easy_setopt
 const libcurl = Process.getModuleByName("libcurl.dll");
 const curl_easy_setopt = Module.getExportByName("libcurl.dll", "curl_easy_setopt");
 
-// Hook into curl_easy_setopt
 Interceptor.attach(curl_easy_setopt, {
     onEnter: function (args) {
-        // CURLOPT_URL is usually 10002
         const CURLOPT_URL = 10002;
         if (parseInt(args[1]) === CURLOPT_URL) {
             const urlPtr = args[2];
             const url = Memory.readUtf8String(urlPtr);
-
-            // Deny/block requests to specific endpoints
-            if (isDeniedEndpoint(url)) {
-                console.log("Blocking", url);
-                this.returnValue = ptr(3);
-                return;
-            }
-
-            if (isTargetUrl(url)) {
-                const newUrl = replaceUrl(url);
-                console.log("Redirecting", url, "to", newUrl);
+            const newUrl = getRedirectUrl(url);
+            if (newUrl) {
+                console.log("[Frida] Redirecting", url, "to", newUrl);
                 Memory.writeUtf8String(urlPtr, newUrl);
             }
         }
