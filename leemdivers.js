97550f684c7a72283fa7dc48d78108dec29f7a07
leem919 <leem919@gmail.com>
2025-05-30 23:34:28 -0700

    Switch to cloudflare pages

--- leemdivers.js
+++ leemdivers.js
@@ -1,30 +1,28 @@
-// Define addresses
+// Define hosts
 const originalHost = "https://api.live.prod.thehelldiversgame.com/";
-const replacementHost = "https://dancing-starlight-d55eb8.netlify.app/";
-const loginEndpoint = "https://api.live.prod.thehelldiversgame.com/api/Account/Login";
-const loginRedirect = "https://dummyjson.com/c/3029-d29f-4014-9fb4";
+const replacementHost = "https://leemdivers-api-static.pages.dev/";
 
-function getRedirectUrl(url) {
-    if (url === loginEndpoint) {
-        return loginRedirect;
-    } else if (url.indexOf(originalHost) === 0) {
-        return url.replace(originalHost, replacementHost);
-    }
-    return null;
+function isTargetUrl(url) {
+    return url.indexOf(originalHost) === 0;
 }
 
-// Hook into curl_easy_setopt
+function replaceUrl(url) {
+    return url.replace(originalHost, replacementHost);
+}
+
+// Find the curl_easy_setopt function
 const libcurl = Process.getModuleByName("libcurl.dll");
 const curl_easy_setopt = Module.getExportByName("libcurl.dll", "curl_easy_setopt");
 
+// Hook into curl_easy_setopt
 Interceptor.attach(curl_easy_setopt, {
     onEnter: function (args) {
         const CURLOPT_URL = 10002;
         if (parseInt(args[1]) === CURLOPT_URL) {
             const urlPtr = args[2];
             const url = Memory.readUtf8String(urlPtr);
-            const newUrl = getRedirectUrl(url);
-            if (newUrl) {
+            if (isTargetUrl(url)) {
+                const newUrl = replaceUrl(url);
                 console.log("[Frida] Redirecting", url, "to", newUrl);
                 Memory.writeUtf8String(urlPtr, newUrl);
             }
